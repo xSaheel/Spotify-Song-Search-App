37a31156b3a22fe0f721fcf1a1fe5dfce0c61c5d
Saheel Kumar Das <56691452+xSaheel@users.noreply.github.com>
2021-04-06 12:38:05 +0530

    Update LoginNav.js
    added deployment url in redirect url

--- src/components/LoginNav.js
+++ src/components/LoginNav.js
@@ -6,7 +6,7 @@ const LoginNav = () => {
 
     const REACT_APP_CLIENT_ID="995b209d3c304fafbdf12221b6bf0857"
     const REACT_APP_AUTHORIZE_URL="https://accounts.spotify.com/authorize"
-    const REACT_APP_REDIRECT_URL="http://localhost:3000/redirect"
+    const REACT_APP_REDIRECT_URL="https://spotify-song-search-app.herokuapp.com/"
     
     const handleLogin = () => {
       window.location = `${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${REACT_APP_REDIRECT_URL}&response_type=token&show_dialog=true`;
