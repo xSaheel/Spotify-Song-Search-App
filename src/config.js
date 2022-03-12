ea3c369f66e9f25b435d1603688db134bfd7a562
Saheel Das <saheelkumar.das@bajajfinserv.in>
2022-03-12 23:03:35 +0530

    added prod redirect url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export const REACT_APP_CLIENT_ID="995b209d3c304fafbdf12221b6bf0857";
 export const REACT_APP_AUTHORIZE_URL="https://accounts.spotify.com/authorize";
-export const REACT_APP_REDIRECT_URL="http://localhost:3000";
+export const REACT_APP_REDIRECT_URL="https://spotify-song-search-app.herokuapp.com";
+// export const REACT_APP_REDIRECT_URL="http://localhost:3000";
