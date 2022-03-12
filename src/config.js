c8f60db77669ff8839eab378be1748858760632c
Saheel Das <saheelkumar.das@bajajfinserv.in>
2022-03-12 23:08:39 +0530

    updated prod client id

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const REACT_APP_CLIENT_ID="995b209d3c304fafbdf12221b6bf0857";
+export const REACT_APP_CLIENT_ID="89e0a607e2ad4c1d9d4e7ce01477d4ba";
 export const REACT_APP_AUTHORIZE_URL="https://accounts.spotify.com/authorize";
 export const REACT_APP_REDIRECT_URL="https://spotify-song-search-app.herokuapp.com";
 // export const REACT_APP_REDIRECT_URL="http://localhost:3000";
