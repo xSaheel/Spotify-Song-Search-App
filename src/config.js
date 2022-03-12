e1bd067fa8274f4fdd84ea22a914557f443ba33b
Saheel Das <saheelkumar.das@bajajfinserv.in>
2022-03-12 22:09:14 +0530

    added config

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const REACT_APP_CLIENT_ID="995b209d3c304fafbdf12221b6bf0857";
+export const REACT_APP_AUTHORIZE_URL="https://accounts.spotify.com/authorize";
+export const REACT_APP_REDIRECT_URL="http://localhost:3000";
