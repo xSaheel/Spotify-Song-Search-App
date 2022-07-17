28157fde3c088964a93eecb77a17c74e8e10392b
Saheel Das <saheelkumar.das@bajajfinserv.in>
2022-07-17 21:17:54 +0530

    changed prod config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,12 @@
 // export const REACT_APP_CLIENT_ID="995b209d3c304fafbdf12221b6bf0857";
 export const REACT_APP_CLIENT_ID="89e0a607e2ad4c1d9d4e7ce01477d4ba";
 export const REACT_APP_AUTHORIZE_URL="https://accounts.spotify.com/authorize";
-// export const REACT_APP_REDIRECT_URL="https://spotify-song-search-app.herokuapp.com/";
-export const REACT_APP_REDIRECT_URL="http://localhost:3000";
-// export const REACT_APP_REDIRECT_URL="https://mini-spotify-clone.netlify.app/";
+
+/**
+ * LOCAL Redirect URL
+ */
+// export const REACT_APP_REDIRECT_URL="http://localhost:3000";
+/**
+ * PROD Redirect URL
+ */
+export const REACT_APP_REDIRECT_URL="https://mini-spotify-clone.netlify.app/";
