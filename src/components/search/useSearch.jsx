9387de675a47552a81382bc26da2d0381d28c4fe
Saheel Das <saheelkumar.das@bajajfinserv.in>
2022-07-17 21:20:37 +0530

    resolving build errors

--- src/components/search/useSearch.jsx
+++ src/components/search/useSearch.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { getArtistDetails, getTopTenSongs } from "../../api";
 
 export const useSearch = (query, limit = 1) => {
